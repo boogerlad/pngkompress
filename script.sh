de8c10a12189aa8d667c1098a61a4d6bdb9c4366
Kevin Hua <kevin.hua@teknically.com>
2014-04-18 22:30:35 -0400

    99% of the way there.

--- script.sh
+++ script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#if folder then count threads and then split workload
-wine truepng -f0,5 -i0 -g0 -a1 -md remove all -zc9 -zm3-9 -zs0-3 -force -y $1
-./pngwolf --in="$1" --out="$1" --exclude-singles --exclude-heuristic --zlib-level=9 --max-stagnate-time=0 --max-evaluations=1 --even-if-bigger
+path=`winepath -w $1`
+wine truepng -f0,5 -i0 -g0 -a1 -md remove all -zc9 -zm3-9 -zs0-3 -force -y "$path"
+wine pngwolfz --in="$path" --out="$path" --exclude-singles --exclude-heuristic --zlib-level=9 --max-stagnate-time=0 --max-evaluations=100 --even-if-bigger
 
 if [[ `pngout -l $1` =~ /c3.*/d([0-9]+) ]]
 then
 	bitdepth=${BASH_REMATCH[1]}
 	basename=`basename $1`
-	pngout -c6 -s4 -q -y $1 /tmp/$basename
-	pngout -c3 -d$bitdepth -n1 -q -y /tmp/$basename
-	pngout -c3 -d$bitdepth -n2 -q -y /tmp/$basename
-	pngout -c3 -d$bitdepth -n3 -q -y /tmp/$basename
-	pngout -c3 -d$bitdepth -n4 -q -y /tmp/$basename
-	pngout -f6 -kp -ks -q -y $1 /tmp/$basename
-	mv /tmp/$basename $1
+	pngout -c6 -s4 -y "$1" "/tmp/$basename"
+	pngout -c3 -d$bitdepth -n1 -y "/tmp/$basename"
+	pngout -c3 -d$bitdepth -n2 -y "/tmp/$basename"
+	pngout -c3 -d$bitdepth -n3 -y "/tmp/$basename"
+	pngout -c3 -d$bitdepth -n4 -y "/tmp/$basename"
+	pngout -f6 -kp -ks -y "$1" "/tmp/$basename"
+	mv "/tmp/$basename" "$1"
 fi
-advdef -z -4 -i 30 -q "$1"
+advdef -z -4 -i 30 "$1"
