47e79875fdcb3a88722efbcf8e1e1a04a2503ef5
Kevin Hua <kevin.hua@teknically.com>
2014-04-20 02:56:31 -0400

    100% added native version of pngwolf without zlib and added check to see if original file is smaller than processed

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+cp "$1" "$1.orig"
 path=`winepath -w "$1"`
 wine truepng -f0,5 -i0 -g0 -a1 -md remove all -zc9 -zm3-9 -zs0-3 -force -y "$path"
-wine pngwolfz --in="$path" --out="$path" --exclude-singles --exclude-heuristic --zlib-level=9 --max-stagnate-time=0 --max-evaluations=1 --even-if-bigger
+./pngwolf --in="$1" --out="$1" --exclude-singles --exclude-heuristic --zlib-level=9 --max-stagnate-time=0 --max-evaluations=1 --even-if-bigger
 
 if [[ `pngout -l "$1"` =~ /c3.*/d([0-9]+) ]]
 then
@@ -13,7 +14,7 @@ then
 	pngout -c3 -d$bitdepth -n2 -y "$1"
 	pngout -c3 -d$bitdepth -n3 -y "$1"
 	pngout -c3 -d$bitdepth -n4 -y "$1"
-	if [[ `stat -c%s "$1.tmp.png"` < `stat -c%s "$1"` ]]
+	if [[ `stat -c%s "$1.tmp.png"` -lt `stat -c%s "$1"` ]]
 	then
 		mv "$1.tmp.png" "$1"
 	else
@@ -33,3 +34,9 @@ mv "$1.tmp" "$1"
 ./defluff <"$1" >"$1.tmp"
 
 mv "$1.tmp" "$1"
+if [[ `stat -c%s "$1.orig"` -lt `stat -c%s "$1"` ]]
+then
+	mv "$1.orig" "$1"
+else
+	rm "$1.orig"
+fi
